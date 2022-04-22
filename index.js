316414266ae4608e95a98f32b8c0adfa7aa46c5b
Carlie Duperval <carlierichell@gmail.com>
2022-04-22 13:23:28 -0400

    Kelvin, celcisus, farre, Project

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function tempConverter(temp){
+    const fahrenheit = (temp * 9/5) + 32
+    const kelvin = (temp+ 274.15)
+    const tempObject = {
+        celcius : temp,
+        fahrenheit : fahrenheit,
+        kelvin : kelvin
+    }
+    return tempObject
+}
+
+console.log(tempConverter(45).fahrenheit)
+console.log(tempConverter(12).kelvin)
+console.log(tempConverter(10).celcius)
